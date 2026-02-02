78033fee087ee68d7857dbcccd773786dbdc6c3d
oadmin <artiom.medvedev2005@yandex.ru>
2026-02-02 14:24:34 +0100

    added car fields

--- Backend-Bot-master/app/schemas/car.py
+++ Backend-Bot-master/app/schemas/car.py
@@ -7,12 +7,18 @@ class CarCreate(BaseSchema):
     driver_profile_id: int = Field(..., gt=0)
     model: str = Field(..., max_length=100)
     number: str = Field(..., max_length=100)
+    region: str = Field(..., max_length=20)
+    vin: str = Field(..., max_length=100)
+    year: str = Field(..., max_length=10)
     created_at: datetime | None = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
 class CarUpdate(BaseSchema):
     model: str | None = Field(None, max_length=100)
     number: str | None = Field(None, max_length=100)
+    region: str | None = Field(None, max_length=20)
+    vin: str | None = Field(None, max_length=100)
+    year: str | None = Field(None, max_length=10)
     updated_at: datetime | None = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
