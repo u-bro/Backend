a78d3c852966a1814b6e1b65fb9370be0cffa10b
oadmin <artiom.medvedev2005@yandex.ru>
2026-03-06 16:14:09 +0100

    changed _calculate_commission_amount method

--- Backend-Bot-master/app/crud/ride.py
+++ Backend-Bot-master/app/crud/ride.py
@@ -49,7 +49,7 @@ class RideCrud(CrudBase[Ride, RideSchema]):
 
     @staticmethod
     def _calculate_commission_amount(expected_fare: float | None, commission: Commission) -> float | None:
-        return commission.fixed_amount + (expected_fare * commission.percentage / 100) if expected_fare else 0
+        return max(commission.fixed_amount, 0.1) + (expected_fare * commission.percentage / 100) if expected_fare else 0.1
 
     @staticmethod
     def _add_commission(data: dict, commission: Commission):
