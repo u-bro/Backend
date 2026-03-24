bf6b0f4e12f295d0e779277347cc0c3187db62c5
oadmin <artiom.medvedev2005@yandex.ru>
2026-03-24 19:26:57 +0100

    added driver-documents/me endpoint

--- Backend-Bot-master/app/crud/driver_document.py
+++ Backend-Bot-master/app/crud/driver_document.py
@@ -1,11 +1,17 @@
 from app.crud.base import CrudBase
 from app.models.driver_document import DriverDocument
 from app.schemas.driver_document import DriverDocumentSchema
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 
 class DriverDocumentCrud(CrudBase[DriverDocument, DriverDocumentSchema]):
     def __init__(self) -> None:
         super().__init__(DriverDocument, DriverDocumentSchema)
 
+    async def get_by_driver_profile_id(self, session: AsyncSession, driver_profile_id: int, **kwargs) -> list[DriverDocumentSchema]:
+        existing = await session.execute(select(self.model).where(self.model.driver_profile_id == driver_profile_id))
+        items = existing.scalars().all()
+        return [self.schema.model_validate(item) for item in items]
 
 driver_document_crud = DriverDocumentCrud()
